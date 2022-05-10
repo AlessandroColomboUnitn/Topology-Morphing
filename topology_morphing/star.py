e3469511d09464bd1cfe1a34f403ed77e107fa1a
alessandro <alessandro.colombo-1@studenti.unitn.it>
2022-05-10 13:22:37 +0000

    Add source. Modify demo outputs.

--- topology_morphing/star.py
+++ topology_morphing/star.py
@@ -15,6 +15,7 @@ from mininet.cli import CLI
 from mininet.util import dumpNodeConnections
 #sfrom nullswitch import NullSwitch
 
+# Node implementing a linux router, source: https://github.com/mininet/mininet/blob/master/examples/linuxrouter.py
 class LinuxRouter( Node ):
     "A Node with IP forwarding enabled."
 
@@ -53,35 +54,31 @@ def defineNet():
     
 
     info( '*** Starting network\n')
-    net.build()
-
-    c0.start()
-    s1.start([c0])
-
+    net.start()
 
     info( '*** Hosts connections:\n')
     dumpNodeConnections(net.hosts)
     info( '\n')
     net.pingAll()
 
-    info( '\n*** Topology Morphing:\n')
+    info( '\n\n*** Topology Morphing:\n')
 
     info( '*** Stopping hosts-switch connections ...\n')
     net.configLinkStatus("h1", "s1", "down")
     net.configLinkStatus("h2", "s1", "down")
     net.configLinkStatus("h3", "s1", "down")
 
-    info( '*** Creating hosts-router links:\n' )
+    info( '*** Creating new hosts-router links:\n' )
     net.addLink( h1, h2, intfName1='h1-eth1', intfName2='h2-eth1', params1={'ip' : "192.168.1.101/24"}, params2={'ip' : "192.168.1.1/24"} ) #params1={'ip' : '192.168.1.101/16'}, params2={ 'ip' : "192.168.1.2/16" })
     net.addLink( h3, h2, intfName1='h3-eth1', intfName2='h2-eth2', params1={'ip' : "192.168.3.101/24"}, params2={'ip' : "192.168.3.1/24"})#, intfName1='h3-eth1', params1={'ip' : '192.168.3.101/16'}, intfName2='h2-eth2', params2={ 'ip' : "192.168.3.2/16" })
     
-    info( '*** Restarting the network:\n')
-    #net.build()
+    info( '*** Changing hosts default routes:\n')
     h1.cmd("ip route add default via 192.168.1.1")
     h3.cmd("ip route add default via 192.168.3.1")
     h2.cmd("ip route add 192.168.1.0 via 192.168.1.1")
     h2.cmd("ip route add 192.168.3.0 via 192.168.1.3")
 
+    info('*** h2 routing table:')
     info( net[ 'h2' ].cmd( 'route' ) )
 
 
@@ -89,9 +86,9 @@ def defineNet():
     dumpNodeConnections(net.hosts)
     net.pingAll()
     #info( '*** Running CLI\n' )
-    CLI( net )
+    #CLI( net )
     
-    info( '*** Stopping network' )
+    info( '*** Stopping network\n' )
     net.stop()
 
 if __name__ == '__main__':
